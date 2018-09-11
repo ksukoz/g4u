856f26bd139ff5aa9920e88df192299b0f2bafba
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-09-11 10:56:47 +0300

    Display commands info in command component

--- src/components/tournaments/Command.js
+++ src/components/tournaments/Command.js
@@ -5,6 +5,7 @@ import compose from "recompose/compose";
 import { FormattedMessage } from "react-intl";
 import { withStyles } from "@material-ui/core/styles";
 
+import Avatar from "@material-ui/core/Avatar";
 import List from "@material-ui/core/List";
 import MenuItem from "@material-ui/core/MenuItem";
 
@@ -20,6 +21,9 @@ const styles = theme => ({
   },
   listItem: {
     border: "1px solid rgba(0,0,0,.2)",
+    "& strong": {
+      padding: "0 2rem"
+    },
     [theme.breakpoints.up("md")]: {
       fontSize: "1.5rem"
     }
@@ -29,6 +33,25 @@ const styles = theme => ({
   },
   error: {
     backgroundColor: "#ff5e5e"
+  },
+  flexDiv: {
+    display: "flex"
+  },
+  centered: {
+    display: "flex",
+    alignSelf: "center",
+    margin: "0 2rem",
+    "& span": {
+      alignSelf: "center",
+      paddingLeft: "2rem",
+      [theme.breakpoints.up("md")]: {
+        fontSize: "1.5rem"
+      }
+    }
+  },
+  avatar: {
+    height: 150,
+    width: 150
   }
 });
 
@@ -41,6 +64,7 @@ class Command extends Component {
     const { classes } = this.props;
     const { commands } = this.props.tournaments;
     let tournamentsList;
+    let lastMatches;
 
     if (commands) {
       tournamentsList = commands.subtours.map(subtour => (
@@ -48,13 +72,47 @@ class Command extends Component {
           {subtour.title}
         </MenuItem>
       ));
+
+      lastMatches = commands.lastgame.map(game => (
+        <MenuItem className={classes.listItem} key={game.game_id}>
+          <span>{game.inTitle}</span>
+          <strong>{game.score}</strong>
+          <span>{game.outTitle}</span>
+        </MenuItem>
+      ));
     }
 
     const id = this.props.match.params.id;
     return (
       <div>
-        <h2>Турниры</h2>
-        <List>{tournamentsList}</List>
+        {commands ? (
+          <div className={classes.flexDiv}>
+            <Avatar
+              alt=""
+              src={commands.info.logo}
+              className={classes.avatar}
+            />
+            <h1 className={classes.centered}>{commands.info.title}</h1>
+            <div className={classes.centered}>
+              <h2>Всего игроков:</h2>
+              <span>{commands.countPl}</span>
+            </div>
+            <div className={classes.centered}>
+              <h2>Средний возраст:</h2>
+              <span>{commands.average}</span>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+        <div>
+          <h2>Последние игры</h2>
+          <List>{lastMatches}</List>
+        </div>
+        <div>
+          <h2>Турниры</h2>
+          <List>{tournamentsList}</List>
+        </div>
       </div>
     );
   }
