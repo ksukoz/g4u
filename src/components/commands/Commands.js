93de9885a82359d3940fe5381ba88f1a803fc1be
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-09-13 14:43:08 +0300

    Display and add links to commands component

--- src/components/commands/Commands.js
+++ src/components/commands/Commands.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import compose from "recompose/compose";
 import { connect } from "react-redux";
 import { withStyles } from "@material-ui/core/styles";
@@ -7,6 +8,9 @@ import { getCommands } from "../../actions/commandActions";
 
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
+
+import List from "@material-ui/core/List";
+import MenuItem from "@material-ui/core/MenuItem";
 
 import SearchIcon from "@material-ui/icons/Search";
 
@@ -56,6 +60,18 @@ const styles = theme => ({
   },
   editor: {
     margin: "1rem 0"
+  },
+  listItem: {
+    border: "1px solid rgba(0,0,0,.2)",
+    height: "auto"
+  },
+  list: {
+    width: "100%",
+    "& *": {
+      [theme.breakpoints.up("md")]: {
+        fontSize: "1.5rem"
+      }
+    }
   }
 });
 
@@ -77,6 +93,44 @@ class Commands extends Component {
 
   render() {
     const { classes } = this.props;
+    const { commands } = this.props.commands;
+    let myCommandsList;
+    let favoriteCommandsList;
+    let allCommandsList;
+
+    if (this.props.commands && commands) {
+      myCommandsList = (
+        <Link to={`/command/${command.cId}`} key={command.cId}>
+          <MenuItem className={classes.listItem}>
+            <img src={commands.my.logo} alt="" />
+            {commands.my.title}
+          </MenuItem>
+        </Link>
+      );
+
+      favoriteCommandsList = commands.liked.map(command => (
+        <Link to={`/command/${command.cId}`} key={command.cId}>
+          <MenuItem className={classes.listItem}>
+            <img src={command.logo} alt="" />
+            {command.title}
+          </MenuItem>
+        </Link>
+      ));
+
+      allCommandsList = commands.all.map(command => (
+        <Link to={`/command/${command.cId}`} key={command.cId}>
+          <MenuItem className={classes.listItem}>
+            <img
+              src={command.logo}
+              alt=""
+              style={{ width: 50, marginRight: 8 }}
+            />
+            {command.title}
+          </MenuItem>
+        </Link>
+      ));
+    }
+
     return (
       <div>
         <TextField
@@ -99,21 +153,29 @@ class Commands extends Component {
         />
         <div>
           <h2>Мои команды</h2>
+          <List className={classes.list}>{myCommandsList}</List>
         </div>
         <div>
           <h2>Любимые команды</h2>
+          <List className={classes.list}>{favoriteCommandsList}</List>
         </div>
         <div>
           <h2>Команды</h2>
+          <List className={classes.list}>{allCommandsList}</List>
         </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  commands: state.commands
+});
+
 export default compose(
   withStyles(styles),
   connect(
-    null,
+    mapStateToProps,
     { getCommands }
   )
 )(Commands);
