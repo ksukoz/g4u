188ec81c3d9ebc96ef3e083a02f75cc4e25dd34b
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-09-13 15:40:33 +0300

    Display command info in command item component

--- src/components/commands/CommandItem.js
+++ src/components/commands/CommandItem.js
@@ -9,6 +9,12 @@ import Avatar from "@material-ui/core/Avatar";
 import List from "@material-ui/core/List";
 import MenuItem from "@material-ui/core/MenuItem";
 
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 import { getCommand } from "../../actions/commandActions";
 
 const styles = theme => ({
@@ -52,6 +58,23 @@ const styles = theme => ({
   avatar: {
     height: 150,
     width: 150
+  },
+  expDetails: {
+    margin: "0 2rem",
+
+    [theme.breakpoints.up("md")]: {
+      fontSize: "1.5rem"
+    }
+  },
+  expSummary: {
+    [theme.breakpoints.up("md")]: {
+      fontSize: "1.5rem"
+    }
+  },
+  link: {
+    width: "100%",
+    textDecoration: "none",
+    color: "#000"
   }
 });
 
@@ -60,7 +83,82 @@ class CommandItem extends Component {
     this.props.getCommand(this.props.match.params.id);
   };
   render() {
-    return <div>hi</div>;
+    const { classes } = this.props;
+    const { command } = this.props.commands;
+    const id = this.props.match.params.id;
+
+    let tournamentsList;
+    let lastMatches;
+    let playersList;
+
+    if (command) {
+      tournamentsList = command.subtours.map(subtour => (
+        <MenuItem className={classes.listItem} key={subtour.tourId}>
+          {subtour.title}
+        </MenuItem>
+      ));
+
+      lastMatches = command.lastgame.map(game => (
+        <MenuItem className={classes.listItem} key={game.game_id}>
+          <span>{game.inTitle}</span>
+          <strong>{game.score}</strong>
+          <span>{game.outTitle}</span>
+        </MenuItem>
+      ));
+
+      playersList = command.players.map(player => (
+        <ExpansionPanelDetails
+          key={player.player_id}
+          className={classes.expDetails}
+        >
+          <Link to={`/player/${player.player_id}`} className={classes.link}>
+            <MenuItem className={classes.listItem}>
+              <img src={player.photo} alt="" />
+              {player.name} ({player.status})
+            </MenuItem>
+          </Link>
+        </ExpansionPanelDetails>
+      ));
+    }
+    return (
+      <div>
+        {command ? (
+          <div className={classes.flexDiv}>
+            <Avatar alt="" src={command.info.logo} className={classes.avatar} />
+            <h1 className={classes.centered}>{command.info.title}</h1>
+            <div className={classes.centered}>
+              <h2>Всего игроков:</h2>
+              <span>{command.countPl}</span>
+            </div>
+            <div className={classes.centered}>
+              <h2>Средний возраст:</h2>
+              <span>{command.average}</span>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+        <div>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              className={classes.expSummary}
+            >
+              Состав команды
+            </ExpansionPanelSummary>
+            {playersList}
+          </ExpansionPanel>
+        </div>
+        <div>
+          <h2>Последние игры</h2>
+          <List>{lastMatches}</List>
+        </div>
+        <div>
+          <h2>Турниры</h2>
+          <List>{tournamentsList}</List>
+        </div>
+      </div>
+    );
   }
 }
 
