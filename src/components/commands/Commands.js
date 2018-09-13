25c9cce9d9a0ce06d4cad2ff65283ae090e4cf3f
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-09-13 14:04:58 +0300

    Add searchbar and block titles to commands component

--- src/components/commands/Commands.js
+++ src/components/commands/Commands.js
@@ -1,9 +1,108 @@
 import React, { Component } from "react";
+import compose from "recompose/compose";
+import { connect } from "react-redux";
+import { withStyles } from "@material-ui/core/styles";
+
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+
+import SearchIcon from "@material-ui/icons/Search";
+
+const styles = theme => ({
+  root: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  form: {
+    width: "49%"
+  },
+  media: {
+    width: "49%"
+  },
+  img: {
+    width: "100%"
+  },
+  input: {
+    width: "100%"
+  },
+  button: {
+    margin: theme.spacing.unit,
+    background: "transparent",
+    color: "rgba(0,0,0,.5)",
+    transition: ".3s",
+    "&:hover, &:active": {
+      backgroundColor: "#43A047",
+      color: "#fff"
+    }
+  },
+  submit: {
+    backgroundColor: "#43A047",
+    borderRadius: 40,
+    color: "#fff"
+  },
+  chip: {
+    backgroundColor: "#effcf1",
+    marginLeft: "1rem",
+    "&:focus": {
+      backgroundColor: "#effcf1"
+    }
+  },
+  editor: {
+    margin: "1rem 0"
+  }
+});
 
 class Commands extends Component {
+  state = {
+    search: ""
+  };
+
+  onChange = e => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
-    return <div />;
+    const { classes } = this.props;
+    return (
+      <div>
+        <TextField
+          className={classes.input}
+          type="text"
+          name="search"
+          value={this.state.search}
+          onChange={this.onChange}
+          onInput={e => {
+            e.target.value = e.target.value;
+          }}
+          label="Поиск"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            )
+          }}
+        />
+        <div>
+          <h2>Мои команды</h2>
+        </div>
+        <div>
+          <h2>Любимые команды</h2>
+        </div>
+        <div>
+          <h2>Команды</h2>
+        </div>
+      </div>
+    );
   }
 }
-
-export default Commands;
+export default compose(
+  withStyles(styles),
+  connect(
+    null,
+    null
+  )
+)(Commands);
