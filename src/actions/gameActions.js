36f88cfd3bbe73828c7cd14ba77d080e3b277084
ksukoz <seniurbano@gmail.com>
2018-09-24 12:02:28 +0300

    Start working on games component

--- src/actions/gameActions.js
+++ src/actions/gameActions.js
@@ -1,89 +1,91 @@
-import axios from "axios";
-import {
-  GET_CURRENT_GAME,
-  GET_EVENT_SETTINGS,
-  GET_ERRORS,
-  GET_MESSAGES
-} from "../actions/types";
+import axios from 'axios';
+import { GET_CURRENT_GAME, GET_GAME_INFO, GET_EVENT_SETTINGS, GET_ERRORS, GET_MESSAGES } from '../actions/types';
 
-export const addGameEvent = (id, eventData) => dispatch => {
-  axios
-    .post(`http://api.mygame4u.com/game/addevent/${id}`, eventData, {
-      headers: {
-        Authorization: `G4User ${
-          JSON.parse(localStorage.getItem("user")).token
-        }`
-      }
-    })
-    .then(res => {
-      if (res.data.error) {
-        dispatch({
-          type: GET_ERRORS,
-          payload: res.data.message
-        });
-      } else {
-        dispatch({
-          type: GET_MESSAGES,
-          payload: res.data.message
-        });
-      }
-    });
+export const addGameEvent = (id, eventData) => (dispatch) => {
+	axios
+		.post(`http://api.mygame4u.com/game/addevent/${id}`, eventData, {
+			headers: {
+				Authorization: `G4User ${JSON.parse(localStorage.getItem('user')).token}`
+			}
+		})
+		.then((res) => {
+			if (res.data.error) {
+				dispatch({
+					type: GET_ERRORS,
+					payload: res.data.message
+				});
+			} else {
+				dispatch({
+					type: GET_MESSAGES,
+					payload: res.data.message
+				});
+			}
+		});
 };
 
-export const getCurrentGame = id => dispatch => {
-  axios
-    .get(`http://api.mygame4u.com/game/id/${id}`, {
-      headers: {
-        Authorization: `G4User ${
-          JSON.parse(localStorage.getItem("user")).token
-        }`
-      }
-    })
-    .then(res => {
-      dispatch({
-        type: GET_CURRENT_GAME,
-        payload: res.data.answer
-      });
-    });
+export const getCurrentGame = (id) => (dispatch) => {
+	axios
+		.get(`http://api.mygame4u.com/game/id/${id}`, {
+			headers: {
+				Authorization: `G4User ${JSON.parse(localStorage.getItem('user')).token}`
+			}
+		})
+		.then((res) => {
+			dispatch({
+				type: GET_CURRENT_GAME,
+				payload: res.data.answer
+			});
+		});
 };
 
-export const getEventSettings = id => dispatch => {
-  axios
-    .get(`http://api.mygame4u.com/game/event/${id}`, {
-      headers: {
-        Authorization: `G4User ${
-          JSON.parse(localStorage.getItem("user")).token
-        }`
-      }
-    })
-    .then(res => {
-      dispatch({
-        type: GET_EVENT_SETTINGS,
-        payload: res.data.answer
-      });
-    });
+export const getGameInfo = (id) => (dispatch) => {
+	axios
+		.get(`http://api.mygame4u.com/game/info/${id}`, {
+			headers: {
+				Authorization: `G4User ${JSON.parse(localStorage.getItem('user')).token}`
+			}
+		})
+		.then((res) => {
+			dispatch({
+				type: GET_GAME_INFO,
+				payload: res.data.answer
+			});
+		});
 };
 
-export const deleteEvent = id => dispatch => {
-  axios
-    .get(`http://api.mygame4u.com/game/delevent/${id}`, {
-      headers: {
-        Authorization: `G4User ${
-          JSON.parse(localStorage.getItem("user")).token
-        }`
-      }
-    })
-    .then(res => {
-      if (res.data.error) {
-        dispatch({
-          type: GET_ERRORS,
-          payload: res.data.message
-        });
-      } else {
-        dispatch({
-          type: GET_MESSAGES,
-          payload: res.data.message
-        });
-      }
-    });
+export const getEventSettings = (id) => (dispatch) => {
+	axios
+		.get(`http://api.mygame4u.com/game/event/${id}`, {
+			headers: {
+				Authorization: `G4User ${JSON.parse(localStorage.getItem('user')).token}`
+			}
+		})
+		.then((res) => {
+			dispatch({
+				type: GET_EVENT_SETTINGS,
+				payload: res.data.answer
+			});
+		});
+};
+
+export const deleteEvent = (id) => (dispatch) => {
+	axios
+		.get(`http://api.mygame4u.com/game/delevent/${id}`, {
+			headers: {
+				Authorization: `G4User ${JSON.parse(localStorage.getItem('user')).token}`
+			}
+		})
+		.then((res) => {
+			if (res.data.error) {
+				dispatch({
+					type: GET_ERRORS,
+					payload: res.data.message
+				});
+			} else {
+				dispatch({
+					type: GET_MESSAGES,
+					payload: res.data.message
+				});
+			}
+		});
 };
--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,23 +1,29 @@
-import { GET_CURRENT_GAME, GET_EVENT_SETTINGS } from "../actions/types";
+import { GET_CURRENT_GAME, GET_GAME_INFO, GET_EVENT_SETTINGS } from '../actions/types';
 
 const initialState = {
-  currentGame: null,
-  settings: null
+	currentGame: null,
+	gameInfo: null,
+	settings: null
 };
 
 export default function(state = initialState, action) {
-  switch (action.type) {
-    case GET_CURRENT_GAME:
-      return {
-        ...state,
-        currentGame: action.payload
-      };
-    case GET_EVENT_SETTINGS:
-      return {
-        ...state,
-        settings: action.payload
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case GET_CURRENT_GAME:
+			return {
+				...state,
+				currentGame: action.payload
+			};
+		case GET_EVENT_SETTINGS:
+			return {
+				...state,
+				settings: action.payload
+			};
+		case GET_GAME_INFO:
+			return {
+				...state,
+				gameInfo: action.payload
+			};
+		default:
+			return state;
+	}
 }
